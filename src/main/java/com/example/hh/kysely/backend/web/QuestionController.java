500e3ca48a41d4375c0a684623fb7ba365e53a11
Nuuttu <34184542+Nuuttu@users.noreply.github.com>
2021-10-28 10:58:28 +0300

    Update QuestionController.java

--- src/main/java/com/example/hh/kysely/backend/web/QuestionController.java
+++ src/main/java/com/example/hh/kysely/backend/web/QuestionController.java
@@ -1,5 +1,15 @@
 package com.example.hh.kysely.backend.web;
 
-public class QuestionController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@Controller
+public class QuestionController {
+	@RequestMapping("/")
+	public String greeting() {
+		return "hello";
+	}
 }
