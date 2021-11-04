4fc82ee1a44ac231b31702596f34940d97b709b1
jonneaspinen <bgo957@myy.haaga-helia.fi>
2021-11-04 09:36:49 +0200

    change

--- src/main/java/com/example/hh/kysely/backend/web/QuestionController.java
+++ src/main/java/com/example/hh/kysely/backend/web/QuestionController.java
@@ -30,14 +30,14 @@ public class QuestionController {
 	@Autowired
 	private QuizRepository quizrepo;
 	
-	// Hakee tietokannasta kysymykset
+	// SEARCH FOR QUESTIONS
 	@RequestMapping(value="/questions")
 	public String QuestionList (Model model) {
 		model.addAttribute("questions", qrepo.findAll());
 		return "questions";
 	}
 	
-	// REST kyselyiden haku
+	// REST, FIND QUIZZES
 	@RequestMapping(value="/questionsRest", method = RequestMethod.GET)
 	public @ResponseBody List<Quiz> questionListRest() {
 		return (List<Quiz>) quizrepo.findAll();
@@ -49,7 +49,6 @@ public class QuestionController {
 	//TULLAAN EKAN KERRAN
 	@RequestMapping(value = "/questionlist", method = RequestMethod.GET)
 	public String naytaAloitus(@RequestParam(name="question", required=false) String content, Model model) {
-		System.out.println("TULTIIN KONTROLLERIIN");
 		questions = new ArrayList<Question>();
 		Question question = new Question();
 		model.addAttribute("question", question);
@@ -57,12 +56,10 @@ public class QuestionController {
 	}
 	@RequestMapping(value= "/save", method = RequestMethod.GET)
 	public String kasittelePostaus(@ModelAttribute(name="question") Question question, Model model) {
-		System.out.println("OLLAAN KÄSITTELE POSTAUKSESSA");
 		questions.add(question);
 		model.addAttribute("questions", questions);
-		System.out.println(question);
-		System.out.println("questions");
-		//HALUTAAN TYHJÄTÄ LOMAKE SEURAAVAA VARTEN
+		
+		// EMPTYING FORM FOR NEXT ONE
 		question = new Question();
 		model.addAttribute("question", question);
 		
@@ -75,40 +72,27 @@ public class QuestionController {
 		model.addAttribute("quiz", new Quiz());
 		return "index";
 	}
+	
+	// FIND QUIZ BY ID
 	@RequestMapping(value="/quiz/{id}")
 	public String quizPage(@PathVariable("id") Long quizId, Model model) {
 		model.addAttribute("quiz", quizrepo.findById(quizId));
 		model.addAttribute("question", new Question());
 	return "quizpage";
 	}
+	
+	// SAVE QUIZ
 	@RequestMapping(value="/quiz/save")
 	public String quizSave(Quiz quiz) {
 		quizrepo.save(quiz);
 		return "redirect:../";
 	}
+	
+	// ADD QUESTION TO QUIZ
 	@RequestMapping(value="/quiz/{id}/addquestion")
 	public String saveQuestionToQuiz(@PathVariable("id") Long quizId, Question question) {
 		qrepo.save(new Question(question.getContent(), quizrepo.findById(quizId).orElse(null)));
 		String returnUrl = "redirect:/quiz/" + quizId; 
 		return returnUrl;
-	}
-	
-	
-	@RequestMapping(value="/question")
-	public String messages(Model model) {
-		Question q1 = new Question();
-		q1.setContent("Who?");
-		Question q2 = new Question();		
-		q2.setContent("What?");
-		Question q3 = new Question();
-		q3.setContent("Why?");
-
-		List<Question> questions = new ArrayList<Question>();
-		questions.add(q1);
-		questions.add(q2);
-		questions.add(q3);
-
-		model.addAttribute("questions", questions);
-		return "questions";
 	}
 }
