11eb7490eeb6b0ce191179637b7cd4015fbfb662
unknown <miettinen1tuomo@gmail.com>
2021-12-15 14:57:45 +0200

    Update WebSecurityConfig.java

--- src/main/java/com/example/hh/kysely/backend/WebSecurityConfig.java
+++ src/main/java/com/example/hh/kysely/backend/WebSecurityConfig.java
@@ -21,7 +21,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+		.cors()
+        .and()
+        .csrf().disable() // CSRF DISABLE; KORJAA MYÖHEMMIN; POST TOIMII
 		.authorizeRequests().antMatchers("/css/**").permitAll()
+		.and()
+		.authorizeRequests().antMatchers("/rest/**").permitAll()
 		.and()
 		.authorizeRequests().anyRequest().authenticated()
 		.and()
